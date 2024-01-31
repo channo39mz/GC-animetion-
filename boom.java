a32014c2f3592a492cd624e7b82d5444b3ac0a1d
bswap01 <97676643+bswap01@users.noreply.github.com>
2024-02-01 05:00:57 +0700

    Add files via upload

---
+++ boom.java
@@ -0,0 +1,72 @@
+import java.awt.*;
+
+public class boom {
+static void boom_chick(Graphics g){
+//left up
+// First Bezier curve
+DrawALGO.my_Bezier_Curve(g, 105, 79, 105, 75, 115, 55, 130, 60);
+// Second Bezier curve
+DrawALGO.my_Bezier_Curve(g, 110, 77, 90, 85, 90, 100, 102, 110);
+// Third Bezier curve
+DrawALGO.my_Bezier_Curve(g, 102, 110, 95, 100, 100, 150, 140, 120);
+// Fourth Bezier curve
+DrawALGO.my_Bezier_Curve(g, 135, 125, 150, 125, 165, 115, 160, 100);
+// Fifth Bezier curve
+DrawALGO.my_Bezier_Curve(g, 125, 65, 150, 35, 190, 80, 155, 105);
+
+
+//right up
+// First Bezier curve
+DrawALGO.my_Bezier_Curve(g, (int)((405 * 1.2) - 70), (int)(79 * 1.2), (int)((405 * 1.2) - 70), (int)(75 * 1.2), (int)((415 * 1.2) - 70), (int)(55 * 1.2), (int)((430 * 1.2) - 70), (int)(60 * 1.2));
+// Second Bezier curve
+DrawALGO.my_Bezier_Curve(g, (int)((410 * 1.2) - 70), (int)(77 * 1.2), (int)((390 * 1.2) - 70), (int)(85 * 1.2), (int)((390 * 1.2) - 70), (int)(100 * 1.2), (int)((402 * 1.2) - 70), (int)(110 * 1.2));
+// Third Bezier curve
+DrawALGO.my_Bezier_Curve(g, (int)((402 * 1.2) - 70), (int)(110 * 1.2), (int)((395 * 1.2) - 70), (int)(100 * 1.2), (int)((400 * 1.2) - 70), (int)(150 * 1.2), (int)((440 * 1.2) - 70), (int)(120 * 1.2));
+// Fourth Bezier curve
+DrawALGO.my_Bezier_Curve(g, (int)((435 * 1.2) - 70), (int)(125 * 1.2), (int)((450 * 1.2) - 70), (int)(125 * 1.2), (int)((465 * 1.2) - 70), (int)(115 * 1.2), (int)((460 * 1.2) - 70), (int)(100 * 1.2));
+// Fifth Bezier curve
+DrawALGO.my_Bezier_Curve(g, (int)((425 * 1.2) - 70), (int)(65 * 1.2), (int)((450 * 1.2) - 70), (int)(35 * 1.2), (int)((490 * 1.2) - 70), (int)(80 * 1.2), (int)((455 * 1.2) - 70), (int)(105 * 1.2));
+
+//down right
+// First Bezier curve
+DrawALGO.my_Bezier_Curve(g, (int)((405 * 1.2) - 70), (int)((79 * 1.2) + 200), (int)((405 * 1.2) - 70), (int)((75 * 1.2) + 200), (int)((415 * 1.2) - 70), (int)((55 * 1.2) + 200), (int)((430 * 1.2) - 70), (int)((60 * 1.2) + 200));
+// Second Bezier curve
+DrawALGO.my_Bezier_Curve(g, (int)((410 * 1.2) - 70), (int)((77 * 1.2) + 200), (int)((390 * 1.2) - 70), (int)((85 * 1.2) + 200), (int)((390 * 1.2) - 70), (int)((100 * 1.2) + 200), (int)((402 * 1.2) - 70), (int)((110 * 1.2) + 200));
+// Third Bezier curve
+DrawALGO.my_Bezier_Curve(g, (int)((402 * 1.2) - 70), (int)((110 * 1.2) + 200), (int)((395 * 1.2) - 70), (int)((100 * 1.2) + 200), (int)((400 * 1.2) - 70), (int)((150 * 1.2) + 200), (int)((440 * 1.2) - 70), (int)((120 * 1.2) + 200));
+// Fourth Bezier curve
+DrawALGO.my_Bezier_Curve(g, (int)((435 * 1.2) - 70), (int)((125 * 1.2) + 200), (int)((450 * 1.2) - 70), (int)((125 * 1.2) + 200), (int)((465 * 1.2) - 70), (int)((115 * 1.2) + 200), (int)((460 * 1.2) - 70), (int)((100 * 1.2) + 200));
+// Fifth Bezier curve
+DrawALGO.my_Bezier_Curve(g, (int)((425 * 1.2) - 70), (int)((65 * 1.2) + 200), (int)((450 * 1.2) - 70), (int)((35 * 1.2) + 200), (int)((490 * 1.2) - 70), (int)((80 * 1.2) + 200), (int)((455 * 1.2) - 70), (int)((105 * 1.2) + 200));
+
+
+//down
+// First Bezier curve
+DrawALGO.my_Bezier_Curve(g, (int)((405 * 1.2) - 245), (int)((79 * 1.2) + 320), (int)((405 * 1.2) - 245), (int)((75 * 1.2) + 320), (int)((415 * 1.2) - 245), (int)((55 * 1.2) + 320), (int)((430 * 1.2) - 245), (int)((60 * 1.2) + 320));
+// Second Bezier curve
+DrawALGO.my_Bezier_Curve(g, (int)((410 * 1.2) - 245), (int)((77 * 1.2) + 320), (int)((390 * 1.2) - 245), (int)((85 * 1.2) + 320), (int)((390 * 1.2) - 245), (int)((100 * 1.2) + 320), (int)((402 * 1.2) - 245), (int)((110 * 1.2) + 320));
+// Third Bezier curve
+DrawALGO.my_Bezier_Curve(g, (int)((402 * 1.2) - 245), (int)((110 * 1.2) + 320), (int)((395 * 1.2) - 245), (int)((100 * 1.2) + 320), (int)((400 * 1.2) - 245), (int)((150 * 1.2) + 320), (int)((440 * 1.2) - 245), (int)((120 * 1.2) + 320));
+// Fourth Bezier curve
+DrawALGO.my_Bezier_Curve(g, (int)((435 * 1.2) - 245), (int)((125 * 1.2) + 320), (int)((450 * 1.2) - 245), (int)((125 * 1.2) + 320), (int)((465 * 1.2) - 245), (int)((115 * 1.2) + 320), (int)((460 * 1.2) - 245), (int)((100 * 1.2) + 320));
+// Fifth Bezier curve
+DrawALGO.my_Bezier_Curve(g, (int)((425 * 1.2) - 245), (int)((65 * 1.2) + 320), (int)((450 * 1.2) - 245), (int)((35 * 1.2) + 320), (int)((490 * 1.2) - 245), (int)((80 * 1.2) + 320), (int)((455 * 1.2) - 245), (int)((105 * 1.2) + 320));
+
+
+
+
+//left down
+// First Bezier curve
+DrawALGO.my_Bezier_Curve(g, (int)((405 * 1.2) - 420), (int)((79 * 1.2) + 200), (int)((405 * 1.2) - 420), (int)((75 * 1.2) + 200), (int)((415 * 1.2) - 420), (int)((55 * 1.2) + 200), (int)((430 * 1.2) - 420), (int)((60 * 1.2) + 200));
+// Second Bezier curve
+DrawALGO.my_Bezier_Curve(g, (int)((410 * 1.2) - 420), (int)((77 * 1.2) + 200), (int)((390 * 1.2) - 420), (int)((85 * 1.2) + 200), (int)((390 * 1.2) - 420), (int)((100 * 1.2) + 200), (int)((402 * 1.2) - 420), (int)((110 * 1.2) + 200));
+// Third Bezier curve
+DrawALGO.my_Bezier_Curve(g, (int)((402 * 1.2) - 420), (int)((110 * 1.2) + 200), (int)((395 * 1.2) - 420), (int)((100 * 1.2) + 200), (int)((400 * 1.2) - 420), (int)((150 * 1.2) + 200), (int)((440 * 1.2) - 420), (int)((120 * 1.2) + 200));
+// Fourth Bezier curve
+DrawALGO.my_Bezier_Curve(g, (int)((435 * 1.2) - 420), (int)((125 * 1.2) + 200), (int)((450 * 1.2) - 420), (int)((125 * 1.2) + 200), (int)((465 * 1.2) - 420), (int)((115 * 1.2) + 200), (int)((460 * 1.2) - 420), (int)((100 * 1.2) + 200));
+// Fifth Bezier curve
+DrawALGO.my_Bezier_Curve(g, (int)((425 * 1.2) - 420), (int)((65 * 1.2) + 200), (int)((450 * 1.2) - 420), (int)((35 * 1.2) + 200), (int)((490 * 1.2) - 420), (int)((80 * 1.2) + 200), (int)((455 * 1.2) - 420), (int)((105 * 1.2) + 200));
+
+
+    }
+}
